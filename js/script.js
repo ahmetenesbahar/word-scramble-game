769324d2a6f1dc9d53223a5d53f79daca1a879c8
ahmetenesbahar <enesbahar2303@gmail.com>
2022-08-19 17:10:35 +0300

    Word Scramble Game

---
+++ js/script.js
@@ -0,0 +1,155 @@
+const wordText = document.querySelector(".word"),
+  hintText = document.querySelector(".hint span"),
+  timeText = document.querySelector(".time b"),
+  inputField = document.querySelector(".input"),
+  refreshBtn = document.querySelector(".refresh-word"),
+  checkBtn = document.querySelector(".check-word");
+
+// Words/Kelimeler
+const words = [
+  {
+    word: "addition",
+    hint: "The process of adding numbers",
+  },
+  {
+    word: "meeting",
+    hint: "Event in which people come together",
+  },
+  {
+    word: "number",
+    hint: "Math symbol used for counting",
+  },
+  {
+    word: "exchange",
+    hint: "The act of trading",
+  },
+  {
+    word: "canvas",
+    hint: "Piece of fabric for oil painting",
+  },
+  {
+    word: "garden",
+    hint: "Space for planting flower and plant",
+  },
+  {
+    word: "position",
+    hint: "Location of someone or something",
+  },
+  {
+    word: "feather",
+    hint: "Hair like outer covering of bird",
+  },
+  {
+    word: "comfort",
+    hint: "A pleasant feeling of relaxation",
+  },
+  {
+    word: "tongue",
+    hint: "The muscular organ of mouth",
+  },
+  {
+    word: "expansion",
+    hint: "The process of increase or grow",
+  },
+  {
+    word: "country",
+    hint: "A politically identified region",
+  },
+  {
+    word: "group",
+    hint: "A number of objects or persons",
+  },
+  {
+    word: "taste",
+    hint: "Ability of tongue to detect flavour",
+  },
+  {
+    word: "store",
+    hint: "Large shop where goods are traded",
+  },
+  {
+    word: "field",
+    hint: "Area of land for farming activities",
+  },
+  {
+    word: "friend",
+    hint: "Person other than a family member",
+  },
+  {
+    word: "pocket",
+    hint: "A bag for carrying small items",
+  },
+  {
+    word: "needle",
+    hint: "A thin and sharp metal pin",
+  },
+  {
+    word: "expert",
+    hint: "Person with extensive knowledge",
+  },
+  {
+    word: "statement",
+    hint: "A declaration of something",
+  },
+  {
+    word: "second",
+    hint: "One-sixtieth of a minute",
+  },
+  {
+    word: "library",
+    hint: "Place containing collection of books",
+  },
+];
+
+let correctWord, timer;
+
+const initTimer = (maxTime) => {
+  timer = setInterval(() => {
+    if (maxTime > 0) {
+      maxTime--;
+      return (timeText.innerText = maxTime);
+    }
+    clearInterval(timer);
+    alert(
+      `Zaman bitti!${correctWord.toUpperCase()} doğru cevaptı / Time is up! ${correctWord.toUpperCase()} was the correct answer`
+    );
+    initGame();
+  }, 1000);
+};
+
+const initGame = () => {
+  initTimer(30);
+  let randomObj = words[Math.floor(Math.random() * words.length)]; //kelimelerden rastgele obje alma /getting random object from words
+  let wordArray = randomObj.word.split(""); //Splitting each letter of random word / her kelimeyi rastgele harflere böler
+  for (let i = wordArray.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); //rastgele sayı alma /geting random num
+    let temp = wordArray[i];
+    wordArray[i] = wordArray[j];
+    wordArray[j] = temp;
+  }
+
+  wordText.innerText = wordArray.join(""); //ayırdığımız kelimelerin boşluklarını neyle bağlıyacağımızı seçiyoruz/passing shuffled word as word text
+  hintText.innerText = randomObj.hint;
+  correctWord = randomObj.word.toLowerCase();
+  inputField.value = "";
+  inputField.setAttribute("maxlength", correctWord.length);
+  console.log(randomObj);
+};
+initGame();
+
+const checkWord = () => {
+  let userWord = inputField.value.toLocaleLowerCase();
+  console.log(userWord);
+  if (userWord != correctWord)
+    return alert(
+      `Oops! ${userWord} doğru cevap değil / Oops! ${userWord} is not correct word`
+    );
+  alert(
+    `Tebrikler!${userWord.toUpperCase()} doğru cevap / Congrats! ${userWord.toUpperCase()} is correct`
+  );
+  if (!userWord) return alert(`Lütfen bir kelime giriniz / Please enter word`);
+  initGame();
+};
+
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
